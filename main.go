c5ff1f13fdea42c7ed48b3dd3a1e4a4e77d4c0f4
omjigupta <omji.mcs.du.2014@gmail.com>
2018-08-08 20:53:51 +0530

    sample api added

--- main.go
+++ main.go
@@ -3,9 +3,28 @@ package main
 import (
   "fmt"
   "golang-gin-restapi-mysql/db"
+  "github.com/gin-gonic/gin"
 )
 
 func main() {
   fmt.Println("hello world")
   db.Init()
+
+  router := SetupRouter()
+  router.Run(":8081")
+}
+
+
+func SetupRouter() *gin.Engine {
+  router := gin.Default()
+
+  v1 := router.Group("api/v1")
+  {
+    v1.GET("/test", Test)
+  }
+  return router
+}
+
+func Test(c *gin.Context) {
+  c.JSON(200, gin.H {"status": 200, "data": "testing api"})
 }
